79062d33dab5fecd6b411615618bac31379ef256
Doug Seven <doug@dougseven.com>
2016-04-16 18:13:43 -0700

    Updated Lab Code

---
+++ HellowWindowsIoT/HellowWindowsIoT/StartupTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+
+// Add using statements to the GrovePi libraries
+using GrovePi;
+using GrovePi.Sensors;
+using Windows.System.Threading;
+
+namespace HellowWindowsIoT
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        ThreadPoolTimer timer;
+        BackgroundTaskDeferral deferral;
+        ILed led;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+
+            // Connect the LED to digital port 4
+            led = DeviceFactory.Build.Led(Pin.DigitalPin4);
+
+            // Create a timer that will 'tick' every one-second
+            timer = ThreadPoolTimer.CreatePeriodicTimer(this.Timer_Tick, TimeSpan.FromSeconds(1));
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer)
+        {
+            try
+            {
+                led.ChangeState((led.CurrentState == SensorStatus.Off) ? SensorStatus.On : SensorStatus.Off);
+            }
+            catch (Exception ex)
+            {
+                // Swallow the exception
+            }
+        }
+    }
+}
